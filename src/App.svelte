362d0838ddcb1a8c3613c655b012e449ecc4398b
cagdaskemik <108903103+cagdaskemik@users.noreply.github.com>
2023-08-29 00:34:21 +0300

    Perfect if you have stuff at the house and dont know what to eat

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
 	import { Router, Route } from "svelte-routing";
-  import Welcome from "./components/home.svelte";
-  import Main from "./components/new.svelte"; // The new page component you want to navigate to.
+	import Welcome from "./components/home.svelte";
+	import Main from "./components/new.svelte"; // The new page component you want to navigate to.
+	import { inject } from "@vercel/analytics";
+
+	inject();
+	
 </script>
 
 <Router>
